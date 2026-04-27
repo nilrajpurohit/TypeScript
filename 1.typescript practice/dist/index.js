207d385f673c33731ef2aca51afdb3a61aed8c1a
Nil Rajpurohit <nilrajp1318@gmail.com>
2026-04-28 01:20:02 +0530

    Latest merged from dev branch

---
+++ 1.typescript practice/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}! Welcome to TypeScript.`;
+}
+const userName = "Nil";
+const message = greet(userName);
+console.log(message);
+//# sourceMappingURL=index.js.map
